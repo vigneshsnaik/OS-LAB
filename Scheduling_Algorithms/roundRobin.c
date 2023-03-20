a075730605a064b5dad612763ecd72f56e571b2c
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2023-03-20 12:33:53 +0530

    Update roundRobin.c

--- Scheduling_Algorithms/roundRobin.c
+++ Scheduling_Algorithms/roundRobin.c
@@ -8,18 +8,8 @@ int min(int a, int b)
 {
 	return (a > b ? b : a);
 }
-void findavgTime(int n, Process *p)
-{
-	float total_wt = 0, total_tat = 0;
-	for (int i = 0; i < n; i++)
-	{
-		total_tat += p[i].tat;
-		total_wt += p[i].wt;
-	}
-	printf("Average waiting time = %f\n", (float)total_wt / (float)n);
-	printf("Average turn around time = %f\n", (float)total_tat / (float)n);
-}
-void startProcess(Process *p, int n, int q)
+
+void roundRobin(Process *p, int n, int q)
 {
 	int time = 0, cur_pTime;
 	bool done = false;
@@ -45,19 +35,7 @@ void startProcess(Process *p, int n, int q)
 		p[i].wt = p[i].tat - p[i].bt;
 	}
 }
-void printProcessTable(int n, Process *p)
-{
 
-	// PRINT TABLE
-	printf(F_BOLD F_RVID "+----+-----+-----+-----+-----+\n" F_RESET);
-	printf(F_BOLD F_RVID "| PN |  BT |  WT | TAT |  CT |\n" F_RESET);
-	printf(F_BOLD F_RVID "+----+-----+-----+-----+-----+\n" F_RESET);
-	for (int i = 0; i < n; i++)
-	{
-		printf(F_RVID "| %2d | %3d | %3d | %3d | %3d |\n" F_RESET, p[i].pid, p[i].bt, p[i].wt, p[i].tat, p[i].ct);
-	}
-	printf(F_RVID "+----+-----+-----+-----+-----+\n" F_RESET);
-}
 
 int main()
 {
@@ -82,7 +60,7 @@ int main()
 	}
 	printf("Time Quantum : ");
 	scanf("%d", &quantum);
-	startProcess(p, n, quantum);
+	roundRobin(p, n, quantum);
 	printProcessTable(n, p);
 	findavgTime(n, p);
 	return 0;
